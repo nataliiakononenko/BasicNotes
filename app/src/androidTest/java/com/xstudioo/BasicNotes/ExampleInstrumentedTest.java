98b500647373423743f73a871b80e3d53789fa85
Nataliia <nataliia.a.kononenko@gmail.com>
2021-01-24 13:56:37 +0100

    Update package name

--- app/src/androidTest/java/com/xstudioo/BasicNotes/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/xstudioo/BasicNotes/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.xstudioo.simpledatabasedemo", appContext.getPackageName());
+        assertEquals("com.xstudioo.basicnotes", appContext.getPackageName());
     }
 }
